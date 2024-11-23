1d1eae503ac2f9ae5738fed93d335d7237a32e2c
Jeremiah Okyere @ mboRijnland <74938408+jeremiahokyere@users.noreply.github.com>
2024-11-23 18:27:44 +0100

    student class toegevoegd
    ik heb de student class toegevoegd en alle attributen voor de student toegevoegd

---
+++ models.py
@@ -0,0 +1,8 @@
+class Student:
+    def __init__(self, gebruikersnaam, wachtwoord, voornaam, achternaam, leeftijd):
+        self.gebruikersnaam = gebruikersnaam
+        self.wachtwoord = wachtwoord
+        self.voornaam = voornaam
+        self.achternaam = achternaam
+        self.leeftijd = leeftijd
+        self.grades = []
