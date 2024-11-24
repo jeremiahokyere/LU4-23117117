a44017aa0a38e3c6c556cea40791032639f2c65a
Jeremiah Okyere @ mboRijnland <74938408+jeremiahokyere@users.noreply.github.com>
2024-11-24 23:00:15 +0100

    docent testen
    hier heb ik de testen uit de user stories van de docent verwerkt

--- tests.py
+++ tests.py
@@ -53,6 +53,43 @@ class TestApplicatie(unittest.TestCase):
         self.assertIn("description", cijfers[0], "Geen feedback")
         self.assertEqual(cijfers[0]["description"], "Goed gedaan!", "Feedback is incorrect.")
 
+    def test_docent_toets_invoeren(self):
+        """test of de docent een toets kan invoeren."""
+        student = self.studenten["Mike Tyson"]
+        student.add_grade(
+            subject="Engels",
+            grade=8.0,
+            date="02-01-2024",
+            attempts=1,
+            description="Top!",
+            docent=self.docent.name
+        )
+        self.assertEqual(len(student.view_grades()), 2, "toets is niet toegevoegd")
+        self.assertEqual(student.view_grades()[-1]["subject"], "Engels", "toets klopt niet")
+
+    def test_docent_toets_wijzigen(self):
+        """test of de docent een toets kan wijzigen"""
+        student = self.studenten["Mike Tyson"]
+        toets = student.view_grades()[0]
+        toets["grade"] = 6.5
+        toets["description"] = "kan beter"
+        self.assertEqual(toets["grade"], 6.5, "cijfer is niet gewijzigd")
+        self.assertEqual(toets["description"], "kan beter", "feedback is niet gewijzigd")
+
+    def test_docent_toets_verwijderen(self):
+        """test of de docent een toets kan verwijderen"""
+        student = self.studenten["Mike Tyson"]
+        cijfers = student.view_grades()
+        del cijfers[0]
+        self.assertEqual(len(student.view_grades()), 0, "Toets is niet verwijderd")
+
+    def test_docent_overzicht_student_toetsen(self):
+        """test of de docent een overzicht van toetsen van een student kan zien"""
+        student = self.studenten["Mike Tyson"]
+        toetsen = student.view_grades()
+        self.assertEqual(len(toetsen), 1, "toetsen overzicht is leeg")
+        self.assertEqual(toetsen[0]["subject"], "wiskunde", "toets overzicht is verkeerd")
+
 
 
 if __name__ == "__main__":
